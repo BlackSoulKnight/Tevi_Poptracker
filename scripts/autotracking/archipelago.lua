a623593f2a31072d2d20447bd8509b30137cafa7
BlackSoulKnight <44965005+BlackSoulKnight@users.noreply.github.com>
2026-01-26 17:58:56 +0100

    Version increment

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -13,7 +13,7 @@ CUR_INDEX = -1
 SLOT_DATA = nil
 LOCAL_ITEMS = {}
 GLOBAL_ITEMS = {}
-AP_WORLD_VERSION = "0.6.6"
+AP_WORLD_VERSION = "0.6.7"
 
 -- resets an item to its initial state
 function resetItem(item_code, item_type)
